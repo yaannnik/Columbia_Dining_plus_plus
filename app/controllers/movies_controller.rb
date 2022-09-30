127054d698e56f8e245c5f3d6b3da04989c89551
Yi Yang <yy3089@columbia.edu>
2022-09-30 21:28:08 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.with_ratings(params[:ratings])
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+  def self.with_ratings(ratings_list)
+    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+    #  movies with those ratings
+    # if ratings_list is nil, retrieve ALL movies
+    if ratings_list == nil || ratings_list.ength == 0
+      return Movie.all
+    else
+      return Movie.where(rating: ratings_list.keys)
+    end
+  end
 end
