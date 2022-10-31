9f4528f1c5cd61ce1bdc92b8f1b9b203459aa6b7
yaannnik <yaannnikk@gmail.com>
2022-10-31 12:58:03 -0400

    add show page spec test

--- spec/controllers/dishes_controller_spec.rb
+++ spec/controllers/dishes_controller_spec.rb
@@ -43,47 +43,16 @@ RSpec.describe DishesController, :type => :controller do
     end
   end
 
-  # TODO: route for show page
-#   describe "GET the display page for a dish" do
-#     it "renders the show page" do
-#       Dish.delete_all
-#       dish1 = { :name => "dish1", :hall => "hall1", :property => "property1", :calories => 100 }
-#       dish2 = { :name => "dish2", :hall => "hall2", :property => "property2", :calories => 200 }
-#       Dish.create(dish1)
-#       Dish.create(dish2)
-#       get :show, :name => "dish1"
-#       expect(response).to render_template("dishes/show")
-#     end
-#   end
-
-#   describe "GET the edit page" do
-#     it "renders the edit page" do
-#       Movie.delete_all
-#       movie1 = { :id => 1, :title => "movie1", :rating => "R", :director => "director1" }
-#       Movie.create(movie1)
-#       get :edit, :id => 1
-#       expect(response).to render_template("movies/edit")
-#     end
-#   end
-
-#   describe "PUT an existing movie" do
-#     it "redirects to the show page" do
-#       Movie.delete_all
-#       movie1 = { :id => 1, :title => "movie1", :rating => "R", :director => "director1" }
-#       Movie.create(movie1)
-#       post :update, :id => 1, :movie => {:id => 1, :title => "movie1_updated", :rating => "R", :director => "director1"}
-#       expect(response).to redirect_to movie_path(1)
-#     end
-#   end
-
-#   describe "DELETE an existing movie" do
-#     it "redirects to the index page" do
-#       Movie.delete_all
-#       movie1 = { :id => 1, :title => "movie1", :rating => "R", :director => "director1" }
-#       Movie.create(movie1)
-#       delete :destroy, :id => 1
-#       expect(response).to redirect_to movies_path
-#     end
-#   end
+  describe "GET the display page for a dish" do
+    it "renders the show page" do
+      Dish.delete_all
+      dish1 = { :id => 1, :name => "dish1", :hall => "hall1", :property => "property1", :calories => 100 }
+      dish2 = { :id => 2, :name => "dish2", :hall => "hall2", :property => "property2", :calories => 200 }
+      Dish.create(dish1)
+      Dish.create(dish2)
+      get :show, :id => 1
+      expect(response).to render_template("dishes/show")
+    end
+  end
 
 end
